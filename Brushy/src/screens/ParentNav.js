5533f653f988ce2f1f75cbc13906d102ebd438e5
margaritaradeva <u12mr20@abdn.ac.uk>
2024-02-19 15:08:50 +0000

    Displaying default profile pic

--- Brushy/src/screens/ParentNav.js
+++ Brushy/src/screens/ParentNav.js
@@ -5,12 +5,22 @@ import Friends from './Friends';
 import Statistics from './Statistics';
 import Settings from './Settings';
 import { ParentScreen } from '.';
+import { profile } from '../assets';
+import { Image } from 'react-native';
 
 const ParentStack = createNativeStackNavigator();
 export default function ParentNav() {
   return (
     <ParentStack.Navigator>
-      <ParentStack.Screen name="Parents Settings" component={ParentScreen}/>
+      <ParentStack.Screen name="Parents Settings" component={ParentScreen}
+      options={{
+        headerLeft: () => (
+          <Image
+                source={profile}
+                style={{width:40, height:40, marginRight:10}}
+                resizeMode='contain'/>
+        ),
+      }}/>
       <ParentStack.Screen name="Add Friends" component={Search} />
       <ParentStack.Screen name="Friends" component={Friends} />
       <ParentStack.Screen name="Statistics" component={Statistics} />
