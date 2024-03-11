932d8c161df00f5312fc6f2385c8f84edba5983e
margaritaradeva <u12mr20@abdn.ac.uk>
2024-03-11 12:35:26 +0000

    transferred old code logic for log in to the new log in that has UI design

--- Brushy/src/screens/Login2.js
+++ Brushy/src/screens/Login2.js
@@ -1,10 +1,60 @@
-import { View, Text, Button, Image, StyleSheet, StatusBar, TouchableOpacity } from "react-native";
+// Import all of the necessary libraries, screens and components
+import { View, Text, Image, StyleSheet, StatusBar, TouchableOpacity, SafeAreaView, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, ScrollView, Platform } from "react-native";
 import Animated, { FadeIn, FadeInDown, FadeInUp, FadeOut } from "react-native-reanimated"
+import { useState } from 'react';
+import { Switch } from 'react-native-paper'; // For password visibility
 import { backgroundImg, logo } from "../assets";
 import { CustomButton, Input } from "../components";
+import { useGlobally} from '../core'; // Custom components
 
 export default function LoginScreen({navigation}) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [requestError, setRequestError] = useState('');
+    const [seePassword, setSeePassword] = useState(false);
+
+    const login = useGlobally(state => state.login); // Login API call
+
+    // function that checks user input before sending an API call
+    async function onLogIn() {
+        // Clear Previous error
+        setRequestError('')
+        //Check username
+        const noEmail = !email
+        if (noEmail) {
+            setEmailError('Email is required')
+        }
+        //check password
+        const noPassword = !password
+        if (noPassword) {
+            setPasswordError('Password is required')
+        }
+        //break out of this function if there were any issues
+        if (noEmail || noPassword) {
+            return
+        }
+        try{
+            await login({ email: email, password: password }) 
+        } catch (error) {
+            if (error.response && error.response.status === 401) {
+                setRequestError('Invalid email or password.');
+            } else {
+                setRequestError('An unexpected error ocurred. Please try again.')
+              }
+            }
+        }
+    
+
+
     return (
+        <SafeAreaView style={{flex:1}}>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{flex:1}}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView 
+        showsVerticalScrollIndicator={false}
+        style={{flex: 1}}>
         <View style ={styles.container}>
             <StatusBar style="light"/>
             <Image
@@ -20,18 +70,38 @@ export default function LoginScreen({navigation}) {
             </View>
             <View style={styles.formContainer}>
                 <Animated.View 
-                    entering={FadeInDown.duration(1000).springify()}
+                    entering={FadeInDown.duration(2000).springify()}
                     style={styles.forms}>
-                        <Input title="Email" margin="5" />
+                        <Input 
+                            title="Email"
+                            value={email}
+                            error={emailError}
+                            setValue={setEmail}
+                            setError={setEmailError}
+                            clear={setRequestError} />
                 </Animated.View>
                 <Animated.View 
-                    entering={FadeInDown.duration(1000).springify()}
+                    entering={FadeInDown.duration(2000).springify()}
                     style={styles.forms}>
-                        <Input title="Password"/>
+                        <Input 
+                            title="Password"
+                            value={password}
+                            error={passwordError}
+                            setValue={setPassword}
+                            setError={setPasswordError}
+                            clear={setRequestError}
+                            />
                 </Animated.View>
-                <CustomButton title='Log In' />
+                <Animated.View
+                    entering={FadeInDown.duration(2000).springify()}
+                    style={styles.forms}>
+                    { requestError ? (
+                        <Text style={styles.errorText}>{requestError}</Text>
+                    ) : null}
+                    <CustomButton title='Log In' onPress={onLogIn}/>
+                </Animated.View>
                 <Animated.View 
-                    entering={FadeInDown.duration(1000).springify()}
+                    entering={FadeInDown.duration(2000).springify()}
                     style={styles.signUpRow}>
                         <Text>Don't have an account? </Text>
                         <TouchableOpacity onPress={() => navigation.navigate('Sign Up')}>
@@ -40,8 +110,13 @@ export default function LoginScreen({navigation}) {
                 </Animated.View>
             </View>
         </View>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+      </SafeAreaView>
     )
-}
+                    }
+                
 
 const styles = StyleSheet.create({
     // Main container
@@ -85,6 +160,13 @@ const styles = StyleSheet.create({
     },
     signUpText: {
         color:'#00008b'
+    },
+    errorText: {
+        color: '#ff0000',
+        fontSize: 14,
+        textAlign: 'center',
+        width: '100%',
+        marginTop:'3%',
     },
 
 
