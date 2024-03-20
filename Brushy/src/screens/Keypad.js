06476abd3563529a07c89b95f9b51262ecec37bb
margaritaradeva <u12mr20@abdn.ac.uk>
2024-03-20 13:37:31 +0000

    made functionality to set and confirm pin partially working

--- Brushy/src/screens/Keypad.js
+++ Brushy/src/screens/Keypad.js
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View, FlatList, Dimensions } from "react-native";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'; // Icons
 import { MotiView } from 'moti';
+import { api } from "../core";
+
 
 // Keys for the pad
 const digits = [1,2,3,4,5,6,7,8,9,'',0, 'del']
@@ -47,17 +49,68 @@ function PinPad({onPress}) {
         />
 }
 
+
 export default function Pin() {
     const [pin, setPin] = useState([])
+    const [firstPin, setFirstPin] = useState(null)
+    const [confirmingPin, setConfirmingPin] = useState(false);
+    const [loading, setLoading] = useState(true) // used for knowing if the async function has finished execution
+    const [message, setMessage] = useState('')
+    const [isPinSet, setIsPinSet] = useState(false)
+    
+
+    useEffect(() => {
+        // When the component renders check of the pin has ever been set before
+        checkPinStatus()
+        
+    },[])
+    
+    useEffect(() => {
+        if (pin.length === pinLength) {
+            if (!confirmingPin) {
+                setFirstPin(pin)
+                setPin([])
+                setConfirmingPin(true)
+            } else {
+                console.log(firstPin,pin)
+            }
+        }
+    })
+
+
+
+    useEffect (() => {
+        if (confirmingPin) {
+            setMessage('Please confirm your pin')
+        } else if (isPinSet) {
+            setMessage('Please enter your pin');
+        } else {
+            setMessage('Please set up your pin');
+        }
+    },[confirmingPin, isPinSet])
+
+    async function checkPinStatus() {
+        try{
+            const response = await api.get('/application/isPinSet/')
+            setIsPinSet(response.data.is_pin_set)
+            setLoading(false)
+        }catch (error) {
+            console.error("errorrrrr", error)
+        }
+        setLoading(false)
+        
+       
+    }
+
     const handlePress = (item) => {
 
         if (item !== '' && item !== 'del') {
-            if (pin.length === pinLength) return
-            setPin(prevPin => {
-                const newPin = [...prevPin, item]
-                console.log(newPin)
-                return newPin})
-               
+           if (!confirmingPin && pin.length < pinLength){
+            setPin(prevPin => [...prevPin, item])
+           } else if (confirmingPin && pin.length < pinLength) {
+            setPin(prevPin => [...prevPin, item])
+           }
+           
         } else if (item === 'del') {
             setPin(prevPin => {
             const newPin = prevPin.slice(0, prevPin.length - 1 )
@@ -66,10 +119,22 @@ export default function Pin() {
         }
     };
 
+   
+
 
     return (
        
         <View style={styles.container}>
+            {loading? (
+                <Text style={styles.messageText}>Loading</Text>
+            ) :(
+                <View style={styles.message}>
+                    <Text style={styles.messageText}>
+                        {message}
+                    </Text>
+                    </View>
+                )}
+               
             <View style={styles.onRow}>
             {[...Array(pinLength).keys()].map( i => {
                 const isSelected =  i < pin.length;
@@ -115,5 +180,12 @@ const styles = StyleSheet.create({
         // backgroundColor: 'green',
         height: pinTextSize*2,
         alignItems: 'flex-end'
+    },
+    message: {
+        marginBottom:20,
+    },
+    messageText: {
+        fontSize: 20,
+        color: '#000000'
     },
 })
