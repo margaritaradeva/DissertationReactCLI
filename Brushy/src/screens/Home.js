86bd0e84843657de4bbd6753345a9b83d6843810
margaritaradeva <u12mr20@abdn.ac.uk>
2024-04-14 00:35:19 +0100

    added character name

--- Brushy/src/screens/Home.js
+++ Brushy/src/screens/Home.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { StyleSheet, View, Animated, Button } from 'react-native';
-import { CountdownTimer } from '../components';
-import { useGlobally } from '../core';
-import BrushingAnimation from '../components/Toothbrushing.';
+import { StyleSheet, View, Animated, Button, Text, Modal, TouchableOpacity, TextInput, Platform, Dimensions } from 'react-native';
+import { CountdownTimer } from '../components/common';
+import { api, useGlobally } from '../core';
+import { bathroom, bathroomZoomedIn } from '../assets/backgrounds';
+import { BlurView } from '@react-native-community/blur';
+import secure from '../core/secure';
 
 
 export default function Home() {
-  const {dogFullBody, userDetails, getDetails, setImageID, setDogFullBodyImage} = useGlobally()
+  const {dogFullBody, userDetails, getDetails, setDogFullBodyImage} = useGlobally()
+  const [showModalStart,setShowModalStart] =useState(false)
+  const [value, setValue] = useState('')
 const zoomedInDog = require('../assets/zoomedInDog.png');
-const bathroom = require('../assets/bathroom.png');
-const bathroomZoomedIn = require('../assets/background2.png'); // The zoomed-in background image
-const [check, setCheck] = useState(false)
+  
+  // Calculate viewport dimensions
+  const { width, height } = Dimensions.get('window');
+  const vh = height / 100;
+  const vw = width / 100;
+
+  // Define styles using vh and vw
+  const dynamicStyles = StyleSheet.create({
+    image2: {
+      width: 100 * vw, 
+      height: 45 * vh, 
+      position: 'absolute',
+      marginTop: 50 * vh - (45 * vh / 2) + (1 * vh), // Moved down by 5% of vh
+
+      
+    },})
 
 const images = {
   dogFullBody: [
@@ -59,8 +76,15 @@ useEffect(() => {
       const dogBodyImage = images.dogFullBody[photoIndex];
       setDogFullBodyImage(dogBodyImage);
       console.log('Image set to:', dogBodyImage);
+      console.log(userDetails.is_char_name_set)
+    if (userDetails.is_char_name_set === false){
+      setShowModalStart(true)
+    
+  }
     }
   }
+ 
+    
 }, [userDetails]);
   const fadeAnim = useRef(new Animated.Value(0)).current; // Starts at 0
 
@@ -82,10 +106,32 @@ useEffect(() => {
 
   const bathroomZoomedInOpacity = fadeAnim;
 
+
+  async function SetUpName() {
+    try{
+      console.log(value)
+    const credentials = await secure.get('credentials')
+    const response = await api({
+      method: 'POST',
+      url: '/application/updateCharacterName/',
+      data: {
+        email: credentials.email,
+        new_name: value
+      }})
+      setShowModalStart(false)
+} catch (error) {
+      console.log('error')
+    }
+
+  }
   return (
     <View style={styles.container}>
       
-      <CountdownTimer seconds={30}  startTimer={startAnimationAndTimer} onModalClose={resetAnimation}/>
+      {userDetails.character_name? 
+      <CountdownTimer seconds={120}  startTimer={startAnimationAndTimer} onModalClose={resetAnimation} name={userDetails.character_name}/> : (
+        <CountdownTimer seconds={120}  startTimer={startAnimationAndTimer} onModalClose={resetAnimation} name={"there"}/>
+      )}
+      
       <View style={styles.backgroundContainer}>
         <Animated.Image
           source={bathroom}
@@ -104,17 +150,55 @@ useEffect(() => {
           <Animated.Image
             source={zoomedInDog}
             style={[
-              styles.image2,
+              dynamicStyles.image2,
               styles.absolutePosition,
               { opacity: bathroomZoomedInOpacity },
             ]}
             resizeMode='contain'
           />
-          
-  <BrushingAnimation step={'topFrontRight'} />
+{/*           
+  <BrushingAnimation step={'topFrontRight'} /> */}
 
         </View>
       </View>
+      <Modal
+            visible={showModalStart}
+            transparent={true}
+            animationType='slide'
+            onRequestClose={() => setShowModalStart(false)}>
+            <BlurView
+          style={StyleSheet.absoluteFill}
+          blurType="light"
+          blurAmount={10}
+          reducedTransparencyFallbackColor="white"
+        />
+
+        <View style={styles.container}>
+          <View style={styles.modalView}>
+            <Text style={styles.infoText}>Pick a name for your character </Text>
+            <View style={styles.modalButton}>
+              
+            <TextInput
+            placeholder={"Character Name"}
+        placeholderTextColor="gray"
+        autoCapitalize="none"
+        autoComplete="off"
+          style={styles.inputText}
+          value={value}
+         onChangeText={text =>{setValue(text)
+        }}>
+            </TextInput>
+            <TouchableOpacity style={styles.button} onPress={() => SetUpName()}>
+              
+                <Text style={styles.buttonText}>Set my name!</Text>
+              </TouchableOpacity>
+              
+            </View>
+          </View>
+
+        </View>
+            </Modal>
+
     </View>
   );
 }
@@ -149,15 +233,71 @@ const styles = StyleSheet.create({
     marginTop: '58%',
     marginLeft:'12%'
   },
-  image2: {
-    width: 450,
-    height: 450,
-    marginTop: '44%',
-    marginRight: '15%',
-  },
   absolutePosition: {
     position: 'absolute',
     top: 0,
     left: 0,
   },
+  modalView: {
+    margin:10,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width:0,
+      height:2,
+    },
+    shadowOpacity:0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    zIndex:1,
+  },
+  modalButton: {
+    flexDirection: 'column',
+    marginTop: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    backgroundColor: '#2E95CA',
+    padding: 6,
+    marginTop: 20,
+    borderRadius: 10,
+    borderColor: 'black',
+
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex:1
+  },
+  buttonText: {
+   
+      color: '#FFFFFF',
+      textShadowColor: '#000000',
+      fontSize: 16,
+      fontWeight: 'bold'
+  
+  },
+  infoText: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'black'
+  },
+  inputText: {
+    backgroundColor:'white',
+        height: Platform.OS == 'android' ? 40 : 20,
+        paddingVertical: 0,
+        borderColor: '#acacac',
+        borderWidth:1,
+        fontSize: 14,
+        backgroundColor:'#f2f2f2',
+        lineHeight: 14 ,
+        color: 'black',
+        borderRadius: 16, height:52, width:'100%',
+         paddingHorizontal:16,fontSize:16
+  }
 });
